95af3f7dfa89b0735c5933b59c3efe287836467f
makla <makla101562@gmail.com>
2025-05-11 17:36:48 +0200

    weapon select is back

--- Assets/WeaponSelect.cs
+++ Assets/WeaponSelect.cs
@@ -2,50 +2,58 @@ using UnityEngine;
 
 public class WeaponSelect : MonoBehaviour
 {
-    private string[] screneNames = { "GamePaintRoller", "GameDrill", "GameSkull", "GameRecipePrinter", "GameToster", "GamePhone", "GameGamblingGun" };
+    [SerializeField] private WeaponData[] weapons;
+
     [SerializeField] private GameManager gameManager;
 
     public void SelectPaintRoller()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[0];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
 
     public void SelectDrill()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[1];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
 
     public void SelectSkull()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[2];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
     public void SelectRecipePrinter()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[3];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
     public void SelectToster()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[4];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
     public void SelectPhone()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[5];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
     public void SelectGamblingGun()
     {
+        WeaponSelector.Instance.weaponStartData = weapons[6];
         gameManager.sceneToLoad = "Game"; 
         gameManager.StartGame();
     }
     public void SelectRandom()
     {
-        int randomIndex = Random.Range(0, screneNames.Length);
-        // gameManager.sceneToLoad = screneNames[randomIndex];
+        int randomIndex = Random.Range(0, weapons.Length);
+        WeaponSelector.Instance.weaponStartData = weapons[randomIndex];
         gameManager.sceneToLoad = "Game";
         gameManager.StartGame();
     }
